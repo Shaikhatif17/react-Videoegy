c89678e563a660a9e500d1d87069dadc98d892dd
atif shaikh <shaikhatif456a@gmail.com>
2023-05-31 15:18:33 +0530

    done with changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Services from "./components/Services";
-
-import "./styles/App.scss";
-import "./styles/mediaquery.scss"
 
 
 
-
-
+import React from "react";
 
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-       
-      </Routes>
-      <Footer />
-    </Router>
+ <div>
+  <h1>
+    app
+  </h1>
+ </div>
   );
 }
 
