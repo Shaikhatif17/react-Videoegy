01c8c8e3fbce8f04122daf0b6924b73bd686c262
atif shaikh <shaikhatif456a@gmail.com>
2023-06-02 15:34:13 +0530

    done with changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+} from '@chakra-ui/react';
+import {Link} from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
-      
-    </div>
-  )
-}
+   <>
+   <Button>Menu</Button>
+   </>
+  );
+};
 
 export default Header;
