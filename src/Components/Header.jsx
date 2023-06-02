1e89dfb0306e49096092017d5739ede60c208f23
atif shaikh <shaikhatif456a@gmail.com>
2023-06-02 15:57:56 +0530

    done with changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,13 +7,55 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Button,
+  useDisclosure,
+  VStack,
 } from '@chakra-ui/react';
 import {Link} from "react-router-dom";
+import {BiMenuAltLeft} from "react-icons/bi"
 
 const Header = () => {
+  const {isOpen,onOpen,onClose}= useDisclosure()
   return (
    <>
-   <Button>Menu</Button>
+   <Button
+   pos={'fixed'}
+   top={"4"}
+   left={"4"}
+  colorScheme='purple'
+  p={"0"}
+  h={"10"}
+  w={"10"}
+  borderRadius={"full"}
+  onClick={onOpen}
+  >
+    <BiMenuAltLeft size={'20'}/>
+   </Button>
+
+   <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+    <DrawerOverlay/>
+    <DrawerContent>
+      
+      <DrawerCloseButton/>
+      <DrawerHeader>VIDEOEGY</DrawerHeader>
+      <DrawerBody>
+        <VStack>
+          <Button variant={'ghost'} colorScheme='"purple'>
+            <Link to="/">Home</Link>
+          </Button>
+          <Button variant={'ghost'} colorScheme='"purple'>
+            <Link  to="/videos">Videos</Link>
+          </Button>
+          <Button variant={'ghost'} colorScheme='"purple'>
+            <Link to="/videos?category=free">Free videos</Link>
+          </Button>
+          <Button variant={'ghost'} colorScheme='"purple'>
+            <Link  to="/upload">upload video</Link>
+          </Button>
+        </VStack>
+      </DrawerBody>
+    </DrawerContent>
+
+   </Drawer>
    </>
   );
 };
