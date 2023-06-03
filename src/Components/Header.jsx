ef991a6de1e09265bd23e263e5d0df0cfc40074e
atif shaikh <shaikhatif456a@gmail.com>
2023-06-03 16:09:14 +0530

    done with changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,57 +11,92 @@ import {
   VStack,
   HStack,
 } from '@chakra-ui/react';
-import {Link} from "react-router-dom";
-import {BiMenuAltLeft} from "react-icons/bi"
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
 
 const Header = () => {
-  const {isOpen,onOpen,onClose}= useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-   <>
-   <Button
-   pos={'fixed'}
-   top={"4"}
-   left={"4"}
-  colorScheme='purple'
-  p={"0"}
-  h={"10"}
-  w={"10"}
-  borderRadius={"full"}
-  onClick={onOpen}
-  >
-    <BiMenuAltLeft size={'20'}/>
-   </Button>
+    <>
+      <Button
+        zIndex={'overlay'}
+        pos={'fixed'}
+        top={'4'}
+        left={'4'}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
 
-   <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
-    <DrawerOverlay/>
-    <DrawerContent>
-      
-      <DrawerCloseButton/>
-      <DrawerHeader>VIDEOEGY</DrawerHeader>
-      <DrawerBody>
-        <VStack alignItems={'flex-start'}>
-          <Button variant={'ghost'} colorScheme='"purple'>
-            <Link to="/">Home</Link>
-          </Button>
-          <Button variant={'ghost'} colorScheme='"purple'>
-            <Link  to="/videos">Videos</Link>
-          </Button>
-          <Button variant={'ghost'} colorScheme='"purple'>
-            <Link to="/videos?category=free">Free videos</Link>
-          </Button>
-          <Button variant={'ghost'} colorScheme='"purple'>
-            <Link  to="/upload">upload video</Link>
-          </Button>
-        </VStack>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
 
-        <HStack>
-          <Button>ok</Button>
-        </HStack>
-      </DrawerBody>
-    </DrawerContent>
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>VIDEO HUB</DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/'}>Home</Link>
+              </Button>
 
-   </Drawer>
-   </>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
+            </VStack>
+
+            <HStack
+              pos={'absolute'}
+              bottom={'10'}
+              left={'0'}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme={'purple'}>
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                colorScheme={'purple'}
+                variant={'outline'}
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
