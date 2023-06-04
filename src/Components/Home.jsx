ae7e044b8e55677bf98f6ddd9527acc3f5bc7d1a
atif shaikh <shaikhatif456a@gmail.com>
2023-06-04 13:47:01 +0530

    done with changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Box, Heading, Image } from '@chakra-ui/react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from "../assets/1.jpg";
-import img2 from "../assets/2.jpg";
-import img3 from "../assets/3.jpg";
-import img4 from "../assets/4.jpg";
-import img5 from "../assets/5.png";
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
 
 const Home = () => {
-    const MyCarousel=()=>{
-        <Carousel autoPlay infiniteLoop interval={1000} showStatus={false} showThumbs={false} showArrows={false}> </Carousel>
-    }
-  return (
-    <Box  w={"full"} h={"100vh"}>
-        <Image src={img1}/>
-        <Heading bgColor={'blackAlpha.600'} color={'white'}>future is gaming</Heading>
-<MyCarousel/>
-    </Box>
-  )
-}
+  const headingOptions = {
+    pos: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%,-50%)',
+    textTransform: 'uppercase',
+    p: '4',
+    size: '4xl',
+  };
 
-export default Home
+  const myCarousel = () => (
+    
+    <Carousel
+      autoPlay
+      infiniteLoop
+      interval={1000}
+      showStatus={false}
+      showArrows={false}
+      showThumbs={false}
+    >
+<Box w="full" h="100vh">
+  <Image src={img1}/>
+  <Heading bgColor={"blackAlpha.600"} color={"white"}>watch the future</Heading>
+</Box>
+
+
+    </Carousel>
+  );
+  <myCarousel/>
+};
+
+export default Home;
