8797330196f9cd93d8e41ffb8dda24dd64d7ade2
atif shaikh <shaikhatif456a@gmail.com>
2023-06-05 17:00:35 +0530

    style- styled footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,63 @@
-import { Box, Button, HStack, Heading, Input, Stack, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Input,
+  Stack,
+  VStack,
+  Text
+} from '@chakra-ui/react';
 import React from 'react';
-import { AiOutlineSend } from 'react-icons/ai';
+import {  AiOutlineSend } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <Box bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
       <Stack direction={['column', 'row']}>
-        <VStack>
+        <VStack alignItems={'stretch'} w={'full'} px={'4'}>
           <Heading size={'md'} textTransform={'uppercase'}>
             subscribe to get updates
           </Heading>
+
+          <HStack>
+            <Input
+              placeholder="Enter your Email Here..."
+              border={'none'}
+              borderRadius={'none'}
+              outline={'none'}
+              focusBorderColor="none"
+            />
+            <Button
+              p={'0'}
+              colorScheme="purple"
+              variant={'ghost'}
+              borderRadius={'0 20px 20px 0'}
+            >
+              <AiOutlineSend size={20} />
+            </Button>
+          </HStack>
         </VStack>
 
-        <HStack>
-            <Input placeholder='Enter your Email Here...' border={'none'} borderRadius={'none'} outline={'none'} focusBorderColor='none' />
-          <Button p={'0'} colorScheme="purple" variant={'ghost'} borderRadius={'0 20px 20px 0'}>
-            <AiOutlineSend size={20} />
-          </Button>
-        </HStack>
+
+
+        <VStack  w={'full'}
+          borderLeft={['none', '1px solid white']}
+          borderRight={['none', '1px solid white']}>
+        
+<Heading textTransform={'uppercase'} textAlign={'center'}> 
+VIDEOEGY
+</Heading>
+<Text> All Rights Reserved</Text>
+        </VStack>
+
+        <VStack w={'full'} >
+<Heading size={'md'} textTransform={'uppercase'}> Social Media</Heading>
+<Button variant={'link'} colorScheme={'whiteAlpha'}>
+  <a href="https://youtube.com"> Youtube </a>
+</Button>
+
+        </VStack>
       </Stack>
     </Box>
   );
